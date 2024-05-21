04377857100b9a72990d1421489112b91b2538bb
Buket Aytac <buketnuraytac@gmail.com>
2024-05-21 11:28:23 +0300

    profil
    profil ve finito be

--- animAI/lib/Pages/profilepage.dart
+++ animAI/lib/Pages/profilepage.dart
@@ -1,3 +1,4 @@
+import 'loginpage.dart';
 import 'package:animai/Pages/loginpage.dart';
 import 'package:animai/data/auth/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,21 +19,67 @@ class Profilepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profile")),
-      body:Center(
-        child: Column(
-          children: [
-            Text(user?.email ?? "User email"),
-            ElevatedButton(onPressed: (){
-                signOut();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-            }, child: Text("Çıkış"))
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Profil"),
         ),
-      )
+        body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column (
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/profile.png',width: 90,height: 90,),
+                        TextButton(onPressed:  (){}, child: const Text('Profil resmi değiştir'))
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Kullanıcı bilgileri')) ,
+                  const SizedBox(height: 40),
+
+                  Row(
+                    children: [
+                      Expanded(flex: 3, child: Text('İsim',style: Theme.of(context).textTheme.bodySmall, overflow: TextOverflow.ellipsis,)),
+                      Expanded(flex: 5, child: Text('Ayşe Yılmaz',style: Theme.of(context).textTheme.bodyMedium, overflow: TextOverflow.ellipsis,)),
+                      const Expanded(child: Icon(Icons.arrow_forward_outlined,size: 18,))
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    children: [
+                      Expanded(flex: 3, child: Text('Kullanıcı Adı',style: Theme.of(context).textTheme.bodySmall, overflow: TextOverflow.ellipsis,)),
+                      Expanded(flex: 5, child: Text('yilmazayse',style: Theme.of(context).textTheme.bodyMedium, overflow: TextOverflow.ellipsis,)),
+                      const Expanded(child: Icon(Icons.arrow_forward_outlined,size: 18,))
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    children: [
+                      Expanded(flex: 3, child: Text('Email',style: Theme.of(context).textTheme.bodySmall, overflow: TextOverflow.ellipsis,)),
+                      Expanded(flex: 5, child: Text('ayseyilmaz@gmail.com',style: Theme.of(context).textTheme.bodyMedium, overflow: TextOverflow.ellipsis,)),
+                      const Expanded(child: Icon(Icons.arrow_forward_outlined,size: 18,))
+                    ],
+                  ),
+                  const SizedBox(height: 130),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage()),
+                        );
+                      },
+                      child: Text("Çıkış Yap", style: TextStyle(color: Colors.red),)),
+
+                ],
+              ),
+            )
+        )
     );
   }
 }
